e409c62f7f8dc53febf875872789e60e1b342d9e
qiaogy91 <9078759+qiaogy91_admin@user.noreply.gitee.com>
2024-11-15 22:00:18 +0800

    PR

--- src/views/backend/Router/RouterView.vue
+++ src/views/backend/Router/RouterView.vue
@@ -41,32 +41,98 @@
 
   <div class="content">
     <a-table :columns="data.tableColumns" :data="data.tableData">
-      <template #optional="{ record }">
+      <template #ops="{ record }">
         <a-space>
-          <a-button status="warning" @click="route_delete(record.metaId)" type="outline">Delete</a-button>
+          <a-button status="success" @click="route_desc(record.metaId)" type="outline"
+            >编辑
+          </a-button>
+          <a-button status="warning" @click="route_delete(record.metaId)" type="outline"
+          >删除
+          </a-button>
+        </a-space>
+      </template>
+      <template #urgent="{ record }">
+        <a-space size="large">
+          <a-switch v-model="record.urgent_call" @change="urgentChange" />
         </a-space>
       </template>
     </a-table>
   </div>
 
   <div>
-    <a-modal v-model:visible="data.modelVisible" @ok="route_create" @cancel="route_cancel">
-      <template #title> Title </template>
+    <a-modal
+      v-model:visible="data.modelVisible"
+      @ok="route_create"
+      @cancel="route_cancel"
+      fullscreen
+    >
+      <template #title> Title</template>
       <div>
-        <a-form :model="data.routeData" :style="{width:'450px'}" auto-label-width>
-          <a-form-item field="identity" label="identity">
+        <a-form :model="data.routeData" :style="{ width: '600px' }" auto-label-width>
+          <a-form-item field="identity" label="项目标识符">
             <a-input v-model="data.routeData.identity" placeholder="项目标识符" />
           </a-form-item>
 
-
-          <a-form-item field="webhook" label="webhook">
+          <a-form-item field="webhook" label="机器人 Webhook">
             <a-input v-model="data.routeData.webhook_url" placeholder="机器人url" />
           </a-form-item>
-          <a-form-item field="token" label="token">
+          <a-form-item field="token" label="机器人 Token">
             <a-input v-model="data.routeData.webhook_token" placeholder="token" />
           </a-form-item>
+
+          <a-divider orientation="right" style="width: 600px"></a-divider>
+
+          <div>
+            <a-form-item field="urgent_call" label="启用电话加急">
+              <a-checkbox v-model="data.routeData.urgent_call"></a-checkbox>
+            </a-form-item>
+
+            <a-form :model="data.tempUser" :style="{ width: '600px' }">
+              <a-form-item
+                label="新建用户"
+                :content-flex="false"
+                :merge-props="false"
+              >
+                <a-row :gutter="8">
+                  <a-col :span="10">
+                    <a-form-item field="together.firstname" validate-trigger="input" no-style>
+                      <a-input v-model="data.tempUser.username" placeholder="LDAP username" />
+                    </a-form-item>
+                  </a-col>
+                  <a-col :span="10">
+                    <a-form-item field="together.lastname" validate-trigger="input" no-style>
+                      <a-input v-model="data.tempUser.phone" placeholder="phone number" />
+                    </a-form-item>
+                  </a-col>
+                  <a-col :span="4">
+                    <a-button type="primary" @click="addTempUser">
+                      <template #icon>
+                        <icon-plus />
+                      </template>
+                      增加
+                    </a-button>
+                  </a-col>
+                </a-row>
+
+              </a-form-item>
+            </a-form>
+          </div>
+
+          <div>
+            <a-form-item field="urgent_call" label="人员列表">
+
+              <a-space>
+                <div v-for="(item,index) in data.routeData.users" :key="index">
+                  <a-tag closable>{{item.username}}-{{item.phone}}</a-tag>
+                </div>
+              </a-space>
+            </a-form-item>
+
+          </div>
         </a-form>
       </div>
+
+
     </a-modal>
   </div>
 </template>
@@ -87,16 +153,31 @@ const data = reactive({
     { title: '所属项目', dataIndex: 'project_desc' },
     { title: '项目编码', dataIndex: 'project_code' },
     { title: 'WebHook', dataIndex: 'webhook_url' },
-    { title: '详情', slotName: 'optional' }
+    { title: '加急', slotName: 'urgent' },
+    { title: '操作', slotName: 'ops' }
   ],
   tableData: [],
   modelVisible: false,
-  routeData : {
-    identity: "",
-    webhook_url: "",
-    webhook_token: ""
+  routeData: {
+    identity: '',
+    webhook_url: '',
+    webhook_token: '',
+    urgent_call: false,
+    users: []
   },
+  tempUser: {
+    username: '',
+    phone: ''
+  }
 })
+
+const addTempUser = async () =>{
+  data.routeData.users.push(data.tempUser)
+  data.tempUser = {
+    username: '',
+    phone: ''
+  }
+}
 
 const routeQuery = async (t, k) => {
   try {
@@ -105,19 +186,20 @@ const routeQuery = async (t, k) => {
     data.tableData = []
     if (rsp.data.items && rsp.data.items.length > 0) {
       rsp.data.items.forEach((item, idx) => {
+        console.log(item)
         let ctime = new Date(item.meta.created_at * 1000)
         let r = {
           idx: idx,
           created_at: ctime.toLocaleString(),
           identity: item.spec.identity,
           webhook_token: item.spec.webhook_token,
-          webhook_url: item.spec.webhook_url
+          webhook_url: item.spec.webhook_url,
+          urgent_call: item.spec.urgent_call
         }
         if (item.spec.project_spec) {
           r.project_line = item.spec.project_spec.project_line
           r.project_code = item.spec.project_spec.project_code
           r.project_desc = item.spec.project_spec.project_desc
-
         }
         r.metaId = item.meta.id
         data.tableData.push(r)
@@ -132,30 +214,51 @@ const routeQuery = async (t, k) => {
 const route_create = async () => {
   try {
     await ROUTE_CREATE(data.routeData)
-    Message.success("创建成功")
-  }catch (err){
+    Message.success('创建成功')
+  } catch (err) {
     Message.error(err)
     console.log(err)
   }
   routeQuery('1', '')
 }
-const route_delete = async (id)=>{
+const route_delete = async (id) => {
   try {
     let rsp = await ROUTE_DELETE(id)
     console.log(rsp)
-  }catch (err){
+  } catch (err) {
     console.log(err)
     Message.error(err)
   }
   routeQuery('1', '')
 }
+
+
+const route_desc = async (id) =>{
+  // todo   create_update
+  try {
+    let rsp = await ROUTE_QUERY(4, id)
+    data.routeData = rsp.data.items[0].spec
+    data.modelVisible = true
+  }catch (err){
+    console.log(err)
+    Message.error(err)
+  }
+}
+
+
+
 const route_cancel = async () => {
   data.routeData = {}
   data.modelVisible = false
 }
+
 onMounted(() => {
   routeQuery('1', '')
 })
+
+const urgentChange = async (e) => {
+  console.log(e)
+}
 </script>
 
 <style scoped>
